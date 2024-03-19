5a7c6edb4a61c633e08f996be55f0d19ee43d64b
m-lockwood <68917937+m-lockwood@users.noreply.github.com>
2024-03-19 15:40:22 +0000

    Make ephys summary modules compatable with sessions without NI-daq

--- main.m
+++ main.m
@@ -4,48 +4,53 @@ clear
 
 %% CONFIG
 
-Animal_ID = '98';
-input_folder = 'W:\projects\FlexiVexi\behavioural_data';
-output_folder = 'C:\Users\megan\Documents\sjlab\flexible-navigation-task\Data Analysis';
+para = CONFIG;
+Animal_ID = '101';
 
 %% write intermediate variables and save locally
 
 % get list of files containing session-level behavioural data for each mouse
-filelist_behaviour = dir(fullfile(input_folder, Animal_ID,'**', '*experimental-data.csv'));
+filelist_behaviour = dir(fullfile(para.input_folder, Animal_ID,'**', '*experimental-data.csv'));
 sessionlist_behaviour = arrayfun(@(x) extractBetween(x.folder, ...
     strcat(Animal_ID, filesep), filesep),filelist_behaviour); %<-- find a cleaner way of adding this (perhaps add to trial summary)?
 
 % concatenate within-session data from all sessions for animal
 trial_data_mouse = table();
 for sessionNum=1:length(filelist_behaviour)
-    trial_data_session = get_trial_data(fullfile(filelist_behaviour(sessionNum).folder, filelist_behaviour(sessionNum).name));
+    trial_data_session = get_trial_data(fullfile(filelist_behaviour(sessionNum).folder, ...
+        filelist_behaviour(sessionNum).name));
     trial_data_mouse = [trial_data_mouse;trial_data_session];
 end
 
 % save to local directory
-writetable(trial_data_mouse, fullfile(output_folder, strcat('trial_summary_', Animal_ID, '.csv')));
+writetable(trial_data_mouse, fullfile(para.output_folder, 'intermediate_variables', ...
+    strcat('trial_summary_', Animal_ID, '.csv')));
 
 %% summarise session-level information
 
 % 1 behavioural data session summary
-session_summary_behaviour = get_session_summary_behaviour(trial_data_mouse);
+sessions_summary_behaviour = get_session_summary_behaviour(trial_data_mouse);
 
 % 2 ephys data session summary
     
 % 2.1 get list of ephys metadata files for animal ID and corresponding session
     % folder
-filelist_ephys = dir(fullfile(input_folder, Animal_ID,'**', '*settings.xml'));
+filelist_ephys = dir(fullfile(para.input_folder, Animal_ID,'**', '*settings.xml'));
 % 2.2 summarise meta-data from every file in filelist_ephys
-session_summary_ephys = get_session_summary_ephys(Animal_ID, filelist_ephys);
+sessions_summary_ephys = get_session_summary_ephys(Animal_ID, filelist_ephys);
 
 % 3 combine behaviour and ephys session summaries
-session_summary_behaviour.session_folder = sessionlist_behaviour;
-session_summary=outerjoin(session_summary_behaviour, session_summary_ephys, ...
+sessions_summary_behaviour.session_folder = sessionlist_behaviour;
+sessions_summary=outerjoin(sessions_summary_behaviour, sessions_summary_ephys, ...
     'Keys', 'session_folder', 'MergeKeys',true);
 
 % 4 save to local directory
-writetable(session_summary, fullfile(output_folder, strcat('session_summary_', ...
-    Animal_ID, '.csv')));
+writetable(sessions_summary, fullfile(para.output_folder, 'intermediate_variables', ...
+    strcat('session_summary_', Animal_ID, '.csv')));
 
 %% plot session summary information
 
+fig = plot_sessions_summary(para, sessions_summary_behaviour);
+output_folder = fullfile(para.output_folder, 'plot_performance_across_sessions');
+filename = strcat('plot_session_bias_accuracy_', Animal_ID);
+save_figure(fig, output_folder, filename);
---
+++ temp/draft_plot_trials_summary.m
@@ -0,0 +1,91 @@
+clear
+
+para = CONFIG;
+Animal_ID = '101';
+
+% plot params
+titleFontSize=16;
+faceAlpha = 0.15;
+accuracy_ylims = [0.2 1];
+choice_ylims = [0 1];
+
+plot_against_time = false;
+plot_aborted_trials = false;
+w=10; 
+
+trial_data = read_trial_summary(para.output_folder, Animal_ID);
+[sessionIdx,sessionList] = findgroups(cellstr(trial_data.Session_ID));
+
+for sessionNum=1:length(sessionList)
+
+%% get plot variables
+
+Session_ID = sessionList(sessionNum); 
+trial_data_session = trial_data(sessionIdx==sessionNum,:);
+%trial_data_session(trial_data_session.AbortTrial==1,:) = [];
+
+correctTrial = trial_data_session.CorrectTrial;
+correctCompletedTrial = correctTrial; 
+correctCompletedTrial(trial_data_session.AbortTrial==1)=nan;
+
+accuracy_movmean_all_trials = movmean_omitnan(correctTrial, w);
+accuracy_movmean_completed_trials = movmean_omitnan(correctCompletedTrial, w);
+
+choice_bias_movmean = movmean_omitnan(trial_data_session.ChoicePort, w);
+
+if plot_against_time
+    x = NosepokeInTime;
+    x(trial_data_session.AbortTrial==1)=nan;
+else
+    x = 1:height(trial_data_session);
+end
+
+if ~plot_aborted_trials
+    abortTrial = logical(trial_data_session.AbortTrial);
+    x=1:sum(~abortTrial);
+    accuracy_movmean_completed_trials(abortTrial) = [];
+    accuracy_movmean_all_trials(abortTrial) = [];
+    choice_bias_movmean(abortTrial) = [];
+end
+
+%% plot local accuracy and choice bias
+
+fig = figure('Visible','on', 'Position', [278 79 928 883]);
+tl = tiledlayout(2,1);
+tl.Padding = "compact";
+
+title(tl,[strcat("Mouse ", Animal_ID), Session_ID, ""], "FontSize", titleFontSize+2);
+xlabel(tl, 'Trial Number', "FontSize",titleFontSize);
+
+ax1 = nexttile;
+    hold on;
+    title(ax1, strcat('Plot accuracy over sliding window, length m=', num2str(w)), 'FontSize',titleFontSize)
+    pl1 = plot(x, accuracy_movmean_completed_trials, 'LineWidth', 2, 'color', ...
+        para.colour_accuracy);
+    pl2 = plot(x, accuracy_movmean_all_trials, 'LineWidth', 2, 'Color', ...
+        para.colour_accuracy, 'LineStyle','--');
+    yline(0.5, '--', "LineWidth",1);
+    ylim(accuracy_ylims)
+    xlim([min(x) max(x)]);
+    ylabel('Fraction of Correct Trials', "FontSize",titleFontSize);
+    legend([pl1 pl2], {'Fraction of Completed Trials', 'Fraction of All Trials'}, ...
+        'FontSize', titleFontSize, 'Location','southoutside');
+
+ax2 = nexttile;
+    hold on;
+    title(ax2, strcat('Plot local choice bias over sliding window, length m=', num2str(w)), 'FontSize',titleFontSize);
+    pl1 = plot(x, choice_bias_movmean, 'color', para.colour_choice, 'LineWidth', 2);
+    yline(0.5, ':', "LineWidth",1);
+    ylim(choice_ylims)
+    xlim([min(x) max(x)]);
+    ylabel('Choice Ratio Port 1 / Port 0', "FontSize",titleFontSize);
+
+%% save plot
+
+output_folder = fullfile(para.output_folder, 'plot_performance_across_trials', Animal_ID);
+filename = strcat('plot_trial_bias_accuracy_', Animal_ID, '_', Session_ID);
+save_figure(fig, output_folder, filename);
+
+close
+
+end
