00f0d6b16f4092aef588e7143c786b8ffc8d5e28
m-lockwood <68917937+m-lockwood@users.noreply.github.com>
2024-05-30 14:56:29 +0100

    Write python script to create data frame with harp timestamps for all sounds, all nosepokes, dot onset/offsets, and the first nosepoke within non-aborted trials. Write core functions in harp_utils.

--- Python/harp/get_square_wave.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def get_square_wave(df): # make names of x,y variables parameterised 
-    #so function can be generalised to other variables / data frames?
-    # Create a new DataFrame with repeated elements
-    square_wave = {'timestamp': df['timestamp'].repeat(2).tolist()[1:],
-        'state': df['state'].repeat(2).tolist()[:-1]
-        }
-    square_wave = pd.DataFrame(square_wave)
-    return square_wave
